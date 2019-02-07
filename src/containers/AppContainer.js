e2909ea16b9f5bdf216a4afb8b1037bba0fd355a
Kevin Anantha <kevin.anantha@gmail.com>
2019-02-07 19:33:14 +0800

    :heart:

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -3,12 +3,10 @@ import { Container, Header } from 'semantic-ui-react'
 import Tracks from '../components/tracks/Tracks';
 
 const AppContainer = () => (
-  <div>
-    <Container text style={{ marginTop: '7em', marginBottom: '45rem' }}>
-      <Header as='h1'>Semantic UI React Fixed Template</Header>
-      <Tracks />
-    </Container>
-  </div>
+  <Container text style={{ marginTop: '7em', marginBottom: '45rem' }}>
+    <Header as='h1'>Semantic UI React Fixed Template</Header>
+    <Tracks />
+  </Container>
 )
 
 export default AppContainer;
--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 const Context = React.createContext();
 
@@ -9,6 +10,19 @@ export class Provider extends Component {
       { id: 2, track_name: 'qwe' },
     ],
     header: "Top 10 Songs",
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
+          process.env.REACT_MM_KEY
+        }`
+      )
+      .then(res => {
+        console.log(res.data)
+      })
+      .catch(err => console.log(err))
   }
 
   render() {
